6a2378519ece6411ee21dedbabcecc5ef8574859
Enrique <EnriqueB5310@gmail.com>
2024-03-11 09:01:40 -0400

    Initial commit

---
+++ src/main/java/org/barrios/elytraplus/ElytraPlus.java
@@ -0,0 +1,20 @@
+package org.barrios.elytraplus;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElytraPlus extends JavaPlugin implements Listener {
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
